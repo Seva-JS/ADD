3cb018cd586175d91e0914bff75dccf36d8a4f7b
seva <sevavac@gmail.com>
2021-01-09 15:36:32 +0300

    DN

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import './App.css';
+import s from './App.module.css'
 import React from "react";
-import Names from "./Items/Names";
+import {BrowserRouter} from "react-router-dom";
+import {Route} from "react-router";
+import Names from "./Names/Names";
+import User from "./Names/Users/User";
+
 
 function App() {
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <Names/>
-            </header>
-        </div>
+        <BrowserRouter>
+            <div className={s.App}>
+                <header>
+                    <div className={s.App_header}>
+                        <Route path={''} render={() => <Names/>}/>
+                        <Route path={'/User'} render={() => <User/>}/>
+                    </div>
+                </header>
+            </div>
+        </BrowserRouter>
     );
 }
 
---
+++ src/Names/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import s from './Name.module.css'
+import {Route} from "react-router";
+import User from "./Users/User";
+export default class Index extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            check: 'red'
+        }
+        this.checkChange = this.checkChange.bind(this)
+
+    }
+
+
+    checkChange(e) {
+        if (this.state.check === 'red') {
+            this.setState({
+                check: 'green'
+            })
+        } else {
+            this.setState({
+                check: 'red'
+            })
+
+        }
+    }
+    NameProf (props){debugger
+        window.location.href = '/User';
+        return (
+            <Route path={'/User'} render={() => <User name={this.props.name.text} />}/>
+        )
+    }
+        render()
+        {
+            const name = this.props.name;
+            return <div id={name.key}>
+                <p className={s.paragraph} checked={this.state.check} style={{backgroundColor: this.state.check}}>
+                    <input type='checkbox' onChange={(e) => {
+                        this.checkChange(e.target.value)
+                    }}/>
+                    <input type="text" id={name.key} value={name.text} className={s.in} onChange={(e) => {
+                        this.props.setUpdate(e.target.value, name.key)
+                    }}/>
+                    <button className={s.but} onClick={() => {
+                        this.props.deleteName(name)
+                    }}>Del
+                    </button>
+                    <button className={s.but} onClick={event => this.NameProf(this.props.name)}>Prof</button>
+                </p>
+            </div>
+
+        }
+    }
+
