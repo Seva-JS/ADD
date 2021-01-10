ceb677f159a0cb36f8aaea2d640646c01745ee44
seva <sevavac@gmail.com>
2021-01-10 22:30:35 +0300

    Now its live

--- src/Items/Index.js
+++ src/Items/Index.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import s from './Post.module.css'
 
-export default class NamesForMap extends React.Component {
+import {Route} from "react-router";
+import User from "../../../src/Names/Users/User";
+export default class Index extends React.Component {
+
     constructor() {
         super();
         this.state = {
@@ -11,7 +13,8 @@ export default class NamesForMap extends React.Component {
 
     }
 
-    checkChange() {
+
+    checkChange(e) {
         if (this.state.check === 'red') {
             this.setState({
                 check: 'green'
@@ -22,23 +25,31 @@ export default class NamesForMap extends React.Component {
             })
 
         }
-
     }
-
-    render() {
+    NameProf (props){debugger
+        window.location.href = '/User';
+        return (
+            <Route path={'/User'} render={() => <User name={this.props.name.text} />}/>
+        )
+    }
+    render()
+    {
         const name = this.props.name;
-        return <div key={name.key}>
+        return <div id={name.key}>
             <p className={s.paragraph} checked={this.state.check} style={{backgroundColor: this.state.check}}>
-                <input type='checkbox' onChange={this.checkChange}/>
+                <input type='checkbox' onChange={(e) => {
+                    this.checkChange(e.target.value)
+                }}/>
                 <input type="text" id={name.key} value={name.text} className={s.in} onChange={(e) => {
                     this.props.setUpdate(e.target.value, name.key)
                 }}/>
                 <button className={s.but} onClick={() => {
-                    this.props.delName(name)
+                    this.props.deleteName(name)
                 }}>Del
                 </button>
+                <button className={s.but} onClick={event => this.NameProf(this.props.name)}>Prof</button>
             </p>
         </div>
+
     }
 }
-
